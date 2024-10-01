f2884cc261d54ddeed33574c72bae26985328724
hungpt2004 <141483872+hungpt2004@users.noreply.github.com>
2024-10-01 09:40:21 +0700

    Last Update Image

--- flutter_login_api/lib/screens/start_screen.dart
+++ flutter_login_api/lib/screens/start_screen.dart
@@ -15,61 +15,139 @@ class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          ClipRRect(
-            child: Center(
-              child: Image.asset("assets/images/logo.jpg"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.5,
-              child: TextField(
-                onTap: () {},
-                controller: _usernameController,
-                obscureText: false,
-                decoration: InputDecoration(
-                    hintText: "Enter username ...",
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15))),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onTap: () {},
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: "Enter password ...",
-                  prefixIcon: Icon(Icons.key),
-                  suffixIcon: Icon(Icons.remove_red_eye_sharp),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15))),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.black,
-              ),
-              child: TextButton(
-                style: ButtonStyle(
-
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: ListView(
+          children: [
+            // ClipRRect(
+            //   child: Center(
+            //     child: Image.asset("assets/images/logo.jpg"),
+            //   ),
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Image.asset(
+                        'assets/images/google_logo.png', // Đường dẫn đến hình ảnh của Google
+                        height: 30, // Điều chỉnh kích thước hình ảnh
+                      ),
+                    ),
+                  ),
                 ),
-                onPressed: () {},
-                child: Text("Sign In", style: TextStyle(color: Colors.white),),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.transparent),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Icon(Icons.facebook),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: TextField(
+                  onTap: () {},
+                  controller: _usernameController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                      hintText: "Enter username ...",
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: TextField(
+                  onTap: () {},
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password ...",
+                      prefixIcon: Icon(Icons.key),
+                      suffixIcon: Icon(Icons.remove_red_eye_sharp),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black,
+                ),
+                child: TextButton(
+                  style: ButtonStyle(),
+                  onPressed: () {},
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+
+            //Forget password
+            Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40),
+              child: TextButton(
+                  onPressed: () {}, child: Text("Forgot password ?")),
+            ),
+
+            //Button login by google, facebook
+            Padding(
+              padding: const EdgeInsets.all(1),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("DON'T HAVE AN ACCOUNT ?"),
+                  TextButton(onPressed: () {}, child: Text("SIGN UP")),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
