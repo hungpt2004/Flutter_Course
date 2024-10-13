cdb180581fef2b1b519d6089c89e278bb3e1544e
hungpt2004 <141483872+hungpt2004@users.noreply.github.com>
2024-10-13 15:21:36 +0700

    Tutorial Bloc + Cubit

--- flutter_bloc_tutorial/lib/main.dart
+++ flutter_bloc_tutorial/lib/main.dart
@@ -3,31 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_tutorial/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_tutorial/cubit/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 import 'bloc/bloc.dart';
 import 'bloc/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+
 
 //================================================================ BLOC =========================================================================
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter BLoC Example',
-//       home: BlocProvider(
-//         create: (_) => CounterBloc(),
-//         child: const CounterScreen(),
-//       ),
-//     );
-//   }
-// }
-
-//=============================================================== CUBIT =========================================================================
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -36,9 +21,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter BLoC Example',
       home: BlocProvider(
-        create: (_) => CounterCubit(),
-        child: const CounterCubitPage(),
+        create: (_) => CounterBloc(),
+        child: const CounterScreen(),
       ),
     );
   }
+}
+
+//=============================================================== CUBIT =========================================================================
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter BLoC Example',
+//       home: BlocProvider(
+//         create: (_) => CounterCubit(),
+//         child: const CounterCubitPage(),
+//       ),
+//     );
+//   }
+// }
+
+//================================================================================================================================================
+// lib/main.dart
+
+void main() {
+  runApp(MyApp());
 }
