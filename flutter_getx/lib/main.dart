f1898ddc6a351099be819641f41cdd0d27842592
hungpt2004 <141483872+hungpt2004@users.noreply.github.com>
2024-10-14 15:06:46 +0700

    create simple app getx + restAPI

---
+++ flutter_getx/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_getx_restapi/Screen/homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
