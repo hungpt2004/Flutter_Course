a2b31107e02c5dc9ac09b1507122c51dd9b36292
hungpt2004 <141483872+hungpt2004@users.noreply.github.com>
2024-10-15 13:42:06 +0700

    Finished payment with Stripe

---
+++ flutter_payment_stripe/lib/pages/consts.dart
@@ -0,0 +1,2 @@
+const String stripePublishableKey = "pk_test_51Q9zmKRoBpzUIPIOcAFj7Up9Lrpa1Aw4C5aG6eGfeumPDkqFr0fFRqYoaMRKnK3lzuqp74SJntAUZnMMQqd8vMVz00bOaeYMbw";
+const String stripeSecretKey = "sk_test_51Q9zmKRoBpzUIPIOjvulCHjK0KMGJygG8Kn5ZsAYdCpYfQ5BJDGsdH1QOl0YQoC3TT3YNgfyHIOzwQYBq6n4yD0x00QrSvgvXI";
---
+++ flutter_payment_stripe/lib/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../services/stripe_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Stripe Payment Demo",
+        ),
+      ),
+      body: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                StripeService.instance.makePayment();
+              },
+              color: Colors.green,
+              child: const Text(
+                "Purchase",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
